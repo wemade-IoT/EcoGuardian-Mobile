3bd69d39f42e22935a405bb720ef03bec5fe0535
Mathias Jave Diaz <u202212721@upc.edu.pe>
2025-06-18 23:09:56 -0500

    chore: configured firebase app distribution dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.ecoguardian"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -17,6 +18,11 @@ android {
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    dependencies{
+        implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+        implementation("com.google.firebase:firebase-analytics")
     }
 
     defaultConfig {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,13 @@
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
+}
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
